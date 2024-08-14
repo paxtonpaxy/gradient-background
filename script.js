5885e15dcbfcef8a3890ffc6645e769affd82d39
Ivan <risticaivan@gmail.com>
2024-08-14 02:57:51 +0200

    added a button to change the colors

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 let likeCounter = 50;
 let dislikeCounter = 50;
 
-// like and dislike functions
-function like() {
-  console.log('like');
-  likeCounter++;
-  document.querySelector('.like-counter').innerHTML = likeCounter;
+function randomBackgroundColor() {
+  let background = document.getElementsByClassName("background")[0];
+
+  let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+  background.style.backgroundColor = randomColor;
 }
 
-function dislike() {
-  console.log('dislike');
-  dislikeCounter++;
-  document.querySelector('.dislike-counter').innerHTML = dislikeCounter;
-}
-
-// style for the background color based on the counters
+document
+  .getElementById("changeColorButton")
+  .addEventListener("click", randomBackgroundColor);
